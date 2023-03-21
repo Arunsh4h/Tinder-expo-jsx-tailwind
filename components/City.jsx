6252292fde50e0cc8489b6fac6de3f0430e6a5d0
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-03-21 14:33:06 +0530

    rnd

---
+++ components/City.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import Icon from "./Icon";
+import styles, { DARK_GRAY } from "../assets/styles";
+
+const City = () => {
+  return (
+    <TouchableOpacity style={styles.city}>
+      <Text style={styles.cityText}>
+        <Icon name="location-sharp" size={13} color={DARK_GRAY} /> New York
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default City;
